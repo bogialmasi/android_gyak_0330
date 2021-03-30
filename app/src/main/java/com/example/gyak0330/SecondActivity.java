53fe31eb5a5368ba2252a18f149647ec6a84edfa
vagnerdavid <vagnerdavid00@gmail.com>
2021-03-30 14:43:51 +0200

    secondactivity átad megjelenítése

--- app/src/main/java/com/example/gyak0330/SecondActivity.java
+++ app/src/main/java/com/example/gyak0330/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.gyak0330;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView textView1 = findViewById(R.id.textView1);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            textView1.setText(extras.getString("atad"));
+        }
     }
 }
